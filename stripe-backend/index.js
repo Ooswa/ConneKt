cd2cddad82f01731004fc2dcb7d236688c1304c2
Ooswa <134034275+Ooswa@users.noreply.github.com>
2025-07-29 10:55:24 +0500

    Added stripe-backend folder

---
+++ stripe-backend/index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const Stripe = require('stripe');
+const cors = require('cors');
+
+const app = express();
+
+// ✅ DIRECTLY using your actual Stripe secret key here
+const stripe = Stripe('sk_test_51RPejV2U8RTuvVXH935i4eo2IexZzme4MZ8SC8DIwlmnzYn0u2HD9MkhJuIeDeLourfjjNMEUY3Wn1pjLQPWoX1U00CS2yfR1W');
+
+app.use(cors());
+app.use(express.json());
+
+app.post('/create-payment-intent', async (req, res) => {
+  const { amount } = req.body;
+
+  try {
+    const paymentIntent = await stripe.paymentIntents.create({
+      amount,
+      currency: 'pkr',
+      payment_method_types: ['card'],
+    });
+
+    return res.send({ clientSecret: paymentIntent.client_secret });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send({ error: error.message });
+  }
+});
+
+
+app.listen(4242, '0.0.0.0', () => console.log('✅ Server running on port 4242'));
